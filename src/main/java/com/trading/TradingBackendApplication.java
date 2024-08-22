0f5cf0a9ab9cd02b39b46e03c7a858579b511136
tonnymuchui <tonykanyingah@gmail.com>
2024-08-23 02:55:00 +0300

    Initial Commit

---
+++ src/main/java/com/trading/TradingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingBackendApplication.class, args);
+	}
+
+}
