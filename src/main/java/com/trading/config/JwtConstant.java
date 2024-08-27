fedf2944e8719d4f0d1a398be71a1843046341f3
tonnymuchui <tonykanyingah@gmail.com>
2024-08-27 03:14:21 +0300

    Add JwtConstant

---
+++ src/main/java/com/trading/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.trading.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "a9027666-4efc-42b9-a02d-908aec71acc1";
+    public static final String JWT_HEADER = "Authorization";
+}
