7de52a2a56adaf6909d63bea67b5b12c5a44277b
tonnymuchui <tonykanyingah@gmail.com>
2024-09-05 04:27:18 +0300

    Update user

--- src/main/java/com/trading/modal/User.java
+++ src/main/java/com/trading/modal/User.java
@@ -12,6 +12,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String fullName;
+    @Column(unique = true)
     private String email;
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
