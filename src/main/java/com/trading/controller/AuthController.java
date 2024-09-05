6ba7fecfeed0f180cd9fb7bdca1de142ccee1f00
tonnymuchui <tonykanyingah@gmail.com>
2024-09-05 04:25:30 +0300

    Update login

--- src/main/java/com/trading/controller/AuthController.java
+++ src/main/java/com/trading/controller/AuthController.java
@@ -1,30 +1,85 @@
 package com.trading.controller;
 
+import com.trading.config.JwtProvider;
 import com.trading.modal.User;
 import com.trading.repository.UserRepository;
+import com.trading.request.LoginRequest;
+import com.trading.response.AuthResponse;
+import com.trading.service.CustomerUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     private final UserRepository userRepository;
+    private final CustomerUserDetailsService customerUserDetailsService;
+
     @Autowired
-    public AuthController(UserRepository UserRepository) {
+    public AuthController(UserRepository UserRepository, CustomerUserDetailsService customerUserDetailsService) {
         this.userRepository = UserRepository;
+        this.customerUserDetailsService = customerUserDetailsService;
     }
+
     @PostMapping("/signup")
-    public ResponseEntity<User> register(@RequestBody User user) {
+    public ResponseEntity<AuthResponse> register(@RequestBody User user) {
+
+        User isEmailExist = userRepository.findByEmail(user.getEmail());
+        if (isEmailExist != null) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Email already exists");
+        }
         User savedUser = new User();
         savedUser.setEmail(user.getEmail());
         savedUser.setFullName(user.getFullName());
         savedUser.setPassword(user.getPassword());
         User savedUserSaved = userRepository.save(savedUser);
-        return new ResponseEntity<>(savedUserSaved, HttpStatus.CREATED);
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        String jwt = JwtProvider.generateToken(auth);
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setToken(jwt);
+        authResponse.setStatus(true);
+        authResponse.setMessage("Successfully registered");
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+
+        String username = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+
+        Authentication auth = authenticate(username, password);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        String jwt = JwtProvider.generateToken(auth);
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setToken(jwt);
+        authResponse.setStatus(true);
+        authResponse.setMessage("Successfully logged in");
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+    }
+
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails = customerUserDetailsService.loadUserByUsername(username);
+        if (userDetails == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+        if (!password.equals(userDetails.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Wrong password");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 }
