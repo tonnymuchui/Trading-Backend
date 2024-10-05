4d8bbe2479b1838a734d1ed97d09764ad8c2201e
tonnymuchui <tonykanyingah@gmail.com>
2024-10-06 00:41:15 +0300

    Add VerificationCode Entity

---
+++ src/main/java/com/trading/modal/VerificationCode.java
@@ -0,0 +1,20 @@
+package com.trading.modal;
+
+import com.trading.domain.VerificationType;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class VerificationCode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String otp;
+    @OneToOne
+    private User user;
+    private String email;
+    private String mobileNumber;
+    private VerificationType verificationType;
+}
