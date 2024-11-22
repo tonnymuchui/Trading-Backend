8531b1af5d3add9ec0d5fa1725c129c067f788fa
tonnymuchui <tonykanyingah@gmail.com>
2024-11-23 02:11:42 +0300

    Add CoinRepository

---
+++ src/main/java/com/trading/repository/CoinRepository.java
@@ -0,0 +1,9 @@
+package com.trading.repository;
+
+import com.trading.modal.Coin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoinRepository extends JpaRepository<Coin, Long> {
+}
