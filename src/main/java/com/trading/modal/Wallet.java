543e4dcd00d96303e53551aa6c5dab1385dc4458
tonnymuchui <tonykanyingah@gmail.com>
2024-11-23 02:14:28 +0300

    Add Wallet

---
+++ src/main/java/com/trading/modal/Wallet.java
@@ -0,0 +1,17 @@
+package com.trading.modal;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public class Wallet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToOne
+    private User user;
+    private BigDecimal balance;
+}
