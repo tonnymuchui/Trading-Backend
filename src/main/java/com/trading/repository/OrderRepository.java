01bb0f2cbdc3a58f563033317a959cc91e466eef
tonnymuchui <tonykanyingah@gmail.com>
2024-11-23 02:13:13 +0300

    Add OrderRepository

---
+++ src/main/java/com/trading/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package com.trading.repository;
+
+import com.trading.modal.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserId(Long userId);
+}
