1f54d1c1773e06471815bbd096aee7eecc82234d
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:25:09 +0300

    Add EmailService

--- src/main/java/com/trading/service/EmailService.java
+++ src/main/java/com/trading/service/EmailService.java
@@ -4,35 +4,34 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
+import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-    private final JavaMailSender mailSender;
 
     @Autowired
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
+    private JavaMailSender javaMailSender;
 
 
-    public void sendVerificationEmail(String email, String otp) throws MessagingException {
-        MimeMessage mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "utf-8");
+    public void sendVerificationOtpEmail(String userEmail, String otp) throws MessagingException, MailSendException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
 
-        String subject = "verification email";
-        String text = "Your verification code is " + otp;
-        mimeMessageHelper.setSubject(subject);
-        mimeMessageHelper.setText(text, true);
-        mimeMessageHelper.setTo(email);
+
+        String subject = "Account verification";
+        String text = "your account verification code is : " + otp;
+
+        helper.setSubject(subject);
+        helper.setText(text, true);
+        helper.setTo(userEmail);
 
         try {
-            mailSender.send(mimeMessage);
+            javaMailSender.send(mimeMessage);
         } catch (MailException e) {
-            throw new MessagingException("Failed to send verification email", e);
+            throw new MailSendException("Failed to send email");
         }
-
     }
 }
