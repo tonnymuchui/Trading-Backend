2a0e22744acbb40e5c8525ca6eab4472e7e25ea9
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:33:33 +0300

    Add TreadingHistory

---
+++ src/main/java/com/trading/modal/TreadingHistory.java
@@ -0,0 +1,26 @@
+package com.trading.modal;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TreadingHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private double sellingPrice;
+
+    private double buyingPrice;
+
+    @Embedded
+    private Coin coin;
+
+    @ManyToOne
+    private User user;
+}
