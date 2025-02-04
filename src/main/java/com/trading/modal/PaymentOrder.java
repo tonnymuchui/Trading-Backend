8ee6ffbfb455928b1d22b2f11b4e97884b810c29
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:30:54 +0300

    Add PaymentOrder

---
+++ src/main/java/com/trading/modal/PaymentOrder.java
@@ -0,0 +1,28 @@
+package com.trading.modal;
+
+import com.trading.domain.PaymentMethod;
+import com.trading.domain.PaymentOrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PaymentOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long amount;
+
+    private PaymentOrderStatus status = PaymentOrderStatus.PENDING;
+
+    private PaymentMethod paymentMethod;
+
+    @ManyToOne
+    private User user;
+}
