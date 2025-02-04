6653a6875c9c0f267ef402692d801adea7791343
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:26:21 +0300

    Add FunctionResponse

---
+++ src/main/java/com/trading/response/FunctionResponse.java
@@ -0,0 +1,10 @@
+package com.trading.response;
+
+import lombok.Data;
+
+@Data
+public class FunctionResponse {
+    private String functionName;
+    private String currencyName;
+    private String currencyData;
+}
