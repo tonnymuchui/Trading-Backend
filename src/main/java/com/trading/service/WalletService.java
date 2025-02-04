03446f0871e0ea369496ba4cc48bc0d5f3b902e3
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:34:13 +0300

    Add WalletService

--- src/main/java/com/trading/service/WalletService.java
+++ src/main/java/com/trading/service/WalletService.java
@@ -1,13 +1,25 @@
 package com.trading.service;
 
+import com.trading.exception.WalletException;
 import com.trading.modal.Order;
 import com.trading.modal.User;
 import com.trading.modal.Wallet;
 
+import java.math.BigDecimal;
+
 public interface WalletService {
-    Wallet getUserWallet(User user);
-    Wallet addBalance(Wallet wallet, Long amount);
-    Wallet findWalletById(Long id) throws Exception;
-    Wallet walletToWalletTrasfer(User sender, Wallet receiverWallet, Long amount) throws Exception;
-    Wallet payOrderPayment(Order order, User user) throws Exception;
+
+
+    Wallet getUserWallet(User user) throws WalletException;
+
+    public Wallet addBalanceToWallet(Wallet wallet, Long money) throws Exception;
+
+    public Wallet findWalletById(Long id) throws WalletException;
+
+    public Wallet walletToWalletTransfer(User sender,Wallet receiverWallet, Long amount) throws WalletException;
+
+    public Wallet payOrderPayment(Order order, User user) throws WalletException;
+
+
+
 }
