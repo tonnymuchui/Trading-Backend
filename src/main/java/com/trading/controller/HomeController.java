cfe6a22962d072abd4adc45d21d9342543331874
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:26:55 +0300

    Add HomeController

--- src/main/java/com/trading/controller/HomeController.java
+++ src/main/java/com/trading/controller/HomeController.java
@@ -1,12 +1,23 @@
 package com.trading.controller;
 
+import com.trading.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
-    @GetMapping
-    public String home() {
-        return "home";
+public class   HomeController {
+
+    @GetMapping("")
+    public ResponseEntity<ApiResponse> homeController(){
+
+
+        ApiResponse res=new ApiResponse(
+                "welcome to crypto treading platform working fine",
+                true
+        );
+        return new ResponseEntity<ApiResponse>(res,HttpStatus.ACCEPTED);
     }
+
 }
