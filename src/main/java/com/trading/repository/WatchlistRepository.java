0ea90d80d35c1ac7c7f1e95cf88429a6846462c0
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:36:05 +0300

    Add WatchlistRepository

---
+++ src/main/java/com/trading/repository/WatchlistRepository.java
@@ -0,0 +1,10 @@
+package com.trading.repository;
+
+import com.trading.modal.Watchlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WatchlistRepository extends JpaRepository<Watchlist,Long> {
+
+    Watchlist findByUserId(Long userId);
+
+}
