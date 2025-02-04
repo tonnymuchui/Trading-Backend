ff346c3f2ef89afe8718bcb7da53694ce515287c
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:21:21 +0300

    Add Asset Entity

---
+++ src/main/java/com/trading/modal/Asset.java
@@ -0,0 +1,29 @@
+package com.trading.modal;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Asset {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double quantity;
+    private double buyPrice;
+
+    @ManyToOne
+    private Coin coin;
+
+
+
+    @ManyToOne
+    private User user;
+
+
+}
