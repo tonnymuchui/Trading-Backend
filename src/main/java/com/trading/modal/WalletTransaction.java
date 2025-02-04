56ca564a7df28308105b5e49ff78ec607ea1b837
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:35:06 +0300

    Add WalletTransaction

--- src/main/java/com/trading/modal/WalletTransaction.java
+++ src/main/java/com/trading/modal/WalletTransaction.java
@@ -4,20 +4,28 @@ import com.trading.domain.WalletTransactionType;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
-@Data
 @Entity
+@Data
 public class WalletTransaction {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @ManyToOne
     private Wallet wallet;
+
     private WalletTransactionType type;
-    private LocalDateTime timestamp;
-    private String TransferId;
+
+    private LocalDate date;
+
+    private String transferId;
+
     private String purpose;
+
     private Long amount;
+
+
 }
