6bfaf20bcd3b658a8bd96408adc723a2d39e7418
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:25:50 +0300

    UpdatePasswordRequest

--- src/main/java/com/trading/modal/ForgotPasswordToken.java
+++ src/main/java/com/trading/modal/ForgotPasswordToken.java
@@ -1,18 +1,23 @@
 package com.trading.modal;
 
 import com.trading.domain.VerificationType;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 
 @Entity
 @Data
-public class ForgotPassword {
+public class ForgotPasswordToken {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private String id;
+
     @OneToOne
     private User user;
+
     private String otp;
+
     private VerificationType verificationType;
+
     private String sendTo;
 }
--- src/main/java/com/trading/repository/ForgotPasswordRepository.java
+++ src/main/java/com/trading/repository/ForgotPasswordRepository.java
@@ -1,10 +1,10 @@
 package com.trading.repository;
 
-import com.trading.modal.ForgotPassword;
+import com.trading.modal.ForgotPasswordToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ForgotPasswordRepository extends JpaRepository<ForgotPassword, String> {
-    ForgotPassword findByUserId(Long userId);
+public interface ForgotPasswordRepository extends JpaRepository<ForgotPasswordToken, String> {
+    ForgotPasswordToken findByUserId(Long userId);
 }
