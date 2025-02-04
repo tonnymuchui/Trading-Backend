7e6a5cedecaf24e898e198ccc50f1ec4e6d5cc9f
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:28:24 +0300

    Add OrderItem

--- src/main/java/com/trading/modal/OrderItem.java
+++ src/main/java/com/trading/modal/OrderItem.java
@@ -2,20 +2,31 @@ package com.trading.modal;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class OrderItem {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    private double quantity;
+
+    @ManyToOne
+    private Coin coin;
+
+    private double buyPrice;
+
+    private double sellPrice;
+
     @JsonIgnore
     @OneToOne
     private Order order;
-    private double quantity;
-    @ManyToOne
-    private Coin coin;
-    private double buyPrice;
-    private double sellPrice;
-    }
+
+}
