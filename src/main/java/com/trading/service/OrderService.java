a71a59f68dbce73d2cd88c2be766e5e65ef0cc01
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:28:52 +0300

    Add OrderService

--- src/main/java/com/trading/service/OrderService.java
+++ src/main/java/com/trading/service/OrderService.java
@@ -9,8 +9,20 @@ import com.trading.modal.User;
 import java.util.List;
 
 public interface OrderService {
+
     Order createOrder(User user, OrderItem orderItem, OrderType orderType);
-    Order getOrderById(Long id);
-    List<Order> getAllOrdersOfUser(Long userId, OrderType orderType, String assetSymbol);
-    Order processOrder(Coin coin, OrderType orderType,double quality, User user) throws Exception;
+
+    Order getOrderById(Long orderId);
+
+    List<Order> getAllOrdersForUser(Long userId, String orderType,String assetSymbol);
+
+    void cancelOrder(Long orderId);
+
+//    Order buyAsset(CreateOrderRequest req, Long userId, String jwt) throws Exception;
+
+    Order processOrder(Coin coin, double quantity, OrderType orderType, User user) throws Exception;
+
+//    Order sellAsset(CreateOrderRequest req,Long userId,String jwt) throws Exception;
+
+
 }
