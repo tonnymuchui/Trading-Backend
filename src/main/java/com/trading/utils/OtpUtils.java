c1875b8137f231d039926eb48d0d82068051071b
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:29:36 +0300

    Add OtpUtils

--- src/main/java/com/trading/utils/OtpUtils.java
+++ src/main/java/com/trading/utils/OtpUtils.java
@@ -3,13 +3,23 @@ package com.trading.utils;
 import java.util.Random;
 
 public class OtpUtils {
+
     public static String generateOTP() {
+
         int otpLength = 6;
-        Random rand = new Random();
+
+
+        Random random = new Random();
+
+
         StringBuilder otp = new StringBuilder(otpLength);
+
+
         for (int i = 0; i < otpLength; i++) {
-            otp.append(rand.nextInt(10));
+            otp.append(random.nextInt(10));
+
         }
+
         return otp.toString();
     }
 }
