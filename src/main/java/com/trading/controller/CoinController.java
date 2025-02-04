0b80581948f0353725d0020be143ff17e32d86b2
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:23:44 +0300

    Add CoinController

--- src/main/java/com/trading/controller/CoinController.java
+++ src/main/java/com/trading/controller/CoinController.java
@@ -1,5 +1,6 @@
 package com.trading.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.trading.modal.Coin;
@@ -14,46 +15,63 @@ import java.util.List;
 @RestController
 @RequestMapping("/coins")
 public class CoinController {
-    private CoinService coinService;
-    private ObjectMapper objectMapper;
 
     @Autowired
-    public CoinController(CoinService coinService, ObjectMapper objectMapper) {
-    }
-    @GetMapping("coin_list")
-    ResponseEntity<List<Coin>> getAllCoins(@RequestParam("page") int page) throws Exception {
-        List<Coin> coins = coinService.getAllCoinList(page);
+    private CoinService coinService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @GetMapping
+    ResponseEntity<List<Coin>> getCoinList(@RequestParam("page") int page) throws Exception {
+        List<Coin> coins=coinService.getCoinList(page);
         return new ResponseEntity<>(coins, HttpStatus.OK);
     }
 
     @GetMapping("/{coinId}/chart")
-    ResponseEntity<JsonNode> getCoinChart(@PathVariable String coinId, @RequestParam("days") int days) throws Exception {
-        String res = coinService.getMarketChart(coinId, days);
-        JsonNode node = objectMapper.readTree(res);
-        return new ResponseEntity<>(node, HttpStatus.ACCEPTED);
-    }
-    @GetMapping("search")
-    ResponseEntity<JsonNode> searchCoin(@RequestParam("query") String query, @RequestParam("page") int page) throws Exception {
-        String coin = coinService.SearchCoin(query);
-        JsonNode jsonNode = objectMapper.readTree(coin);
+    ResponseEntity<JsonNode> getMarketChart(@PathVariable String coinId,
+                                            @RequestParam("days")int days) throws Exception {
+        String coins=coinService.getMarketChart(coinId,days);
+        JsonNode jsonNode = objectMapper.readTree(coins);
+
+
         return ResponseEntity.ok(jsonNode);
+
+    }
+
+
+    @GetMapping("/search")
+    ResponseEntity<JsonNode> searchCoin(@RequestParam("q") String keyword) throws JsonProcessingException {
+        String coin=coinService.searchCoin(keyword);
+        JsonNode jsonNode = objectMapper.readTree(coin);
+
+        return ResponseEntity.ok(jsonNode);
+
     }
     @GetMapping("/top50")
-    ResponseEntity<JsonNode> getTop50CoinsByMarketRank(@RequestParam("page") int page) throws Exception {
-        String coin = coinService.getTop50CoinsByMarketRank();
+    ResponseEntity<JsonNode> getTop50CoinByMarketCapRank() throws JsonProcessingException {
+        String coin=coinService.getTop50CoinsByMarketCapRank();
+        JsonNode jsonNode = objectMapper.readTree(coin);
+
+        return ResponseEntity.ok(jsonNode);
+
+    }
+
+    @GetMapping("/trading")
+    ResponseEntity<JsonNode> getTreadingCoin() throws JsonProcessingException {
+        String coin=coinService.getTreadingCoins();
         JsonNode jsonNode = objectMapper.readTree(coin);
         return ResponseEntity.ok(jsonNode);
+
     }
-    @GetMapping("/treading")
-    ResponseEntity<JsonNode> getTrendingCoinsByMarketRank(@RequestParam("page") int page) throws Exception {
-        String coin = coinService.getTrendingCoinsByMarketRank();
-        JsonNode jsonNode = objectMapper.readTree(coin);
-        return ResponseEntity.ok(jsonNode);
-    }
+
     @GetMapping("/details/{coinId}")
     ResponseEntity<JsonNode> getCoinDetails(@PathVariable String coinId) throws Exception {
-        String res = coinService.getCoinDetails(coinId);
-        JsonNode node = objectMapper.readTree(res);
-        return ResponseEntity.ok(node);
+        String coin=coinService.getCoinDetails(coinId);
+        JsonNode jsonNode = objectMapper.readTree(coin);
+
+        return ResponseEntity.ok(jsonNode);
+
     }
+
 }
