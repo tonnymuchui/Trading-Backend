834b187ce34e626b9013e916a05a3ea3b3091c40
tonnymuchui <tonykanyingah@gmail.com>
2025-02-04 10:35:25 +0300

    Add WalletTransactionRepository

---
+++ src/main/java/com/trading/repository/WalletTransactionRepository.java
@@ -0,0 +1,13 @@
+package com.trading.repository;
+
+import com.trading.modal.Wallet;
+import com.trading.modal.WalletTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface WalletTransactionRepository extends JpaRepository<WalletTransaction,Long> {
+
+    List<WalletTransaction> findByWalletOrderByDateDesc(Wallet wallet);
+
+}
